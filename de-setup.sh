5d1a8ec7c8a1ac362dfc148d5b2b7970834efb73
Robert Ely <robertely@gmail.com>
2016-03-29 22:41:19 -0600

    Fill out de-setup with a minimal desktop

---
+++ de-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "Bro, do you even sudo?" 1>&2
+   exit 1
+fi
+
+# Build apt-cache
+sudo apt-get update
+
+# Install base packages:
+sudo apt-get install -y \
+  vim-nox\
+  git\
+  make\
+  build-essential\
+  python-pip\
+  python-dev\
+  golang\
+  terminator\
+  tree\
+  htop\
+  atop\
+  openvpn\
+  dropbox\
+  transmission\
+  fonts-droid\
+  fonts-noto
+
+# Setup Dropbox while we wait
+dropbox start -i
+
+#Install extras - Jerks with out good repos
+mkdir -p /tmp/de-setup
+wget https://atom-installer.github.com/v1.6.1/atom-amd64.deb -O /tmp/de-setup/atom.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/de-setup/chrome.deb
+sudo dpkg -i /tmp/de-setup/atom.deb
+sudo dpkg -i /tmp/de-setup/chrome.deb
+
+#Setup u2f
+sudo wget https://raw.githubusercontent.com/robertely/libu2f-host/master/70-u2f.rules -O /etc/udev/rules.d/70-u2f.rules
+
+# Set wallpaper
+wget https://raw.githubusercontent.com/robertely/de-setup/master/Pam_Cheryl_RPG.png -O ~/Pictures/Pam_Cheryl_RPG.png
+gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/rely/Pictures/Pam_Cheryl_RPG.png"
