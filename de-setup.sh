60735b18c898c969b1de1868de690444e026681f
Robert Ely <robertely@gmail.com>
2016-11-08 07:04:11 -0700

    update atom url

--- de-setup.sh
+++ de-setup.sh
@@ -32,7 +32,7 @@ dropbox start -i&
 
 #Install extras - Jerks with out good repos
 mkdir -p /tmp/de-setup
-wget https://atom-installer.github.com/v1.6.1/atom-amd64.deb -O /tmp/de-setup/atom.deb
+wget https://atom.io/download/deb -O /tmp/de-setup/atom.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/de-setup/chrome.deb
 wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb -O /tmp/de-setup/steam.deb
 sudo dpkg -i /tmp/de-setup/atom.deb
