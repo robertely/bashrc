0019090fbda27aea48662d3f138bad42bc7b4325
Robert Ely <robertely@gmail.com>
2017-12-30 21:49:31 -0700

    tidy, python3, dialout rights

--- de-setup.sh
+++ de-setup.sh
@@ -20,6 +20,7 @@ sudo apt-get install -y \
   build-essential\
   python-pip\
   python-dev\
+  python3-pip\
   golang\
   terminator\
   tree\
@@ -31,17 +32,13 @@ sudo apt-get install -y \
   fonts-noto-cjk\
   redshift
 
-# Setup Dropbox while we wait
-# dropbox start -i&
 
 #Install extras - Jerks with out good repos
 mkdir -p /tmp/de-setup
 wget https://atom.io/download/deb -O /tmp/de-setup/atom.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/de-setup/chrome.deb
-# wget https://steamcdn-a.akamaihd.net/client/installer/steam.deb -O /tmp/de-setup/steam.deb
 sudo dpkg -i /tmp/de-setup/atom.deb
 sudo dpkg -i /tmp/de-setup/chrome.deb
-# sudo dpkg -i /tmp/de-setup/steam.deb
 
 # Pythoooon
 sudo pip install pep8
@@ -56,13 +53,10 @@ apm install \
   atom-beautify\
   file-icons
   
-# Start Steam while we wait
-# steam&
 
 #Setup u2f
 sudo wget https://raw.githubusercontent.com/robertely/libu2f-host/master/70-u2f.rules -O /etc/udev/rules.d/70-u2f.rules
 
-# Set wallpaper
-wget https://raw.githubusercontent.com/robertely/de-setup/master/Pam_Cheryl_RPG.png -O ~/Pictures/Pam_Cheryl_RPG.png
-# gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/rely/Pictures/Pam_Cheryl_RPG.png"
+#Serial
+sudo adduser rely dialout
 
